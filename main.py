0ae6720815a9d15ee6479459c182f5737c3777db
Cg_progidy <brightmarshall2@gmail.com>
2021-07-24 14:53:49 +0300

    Radio Buttons

---
+++ main.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty
+
+
+class BoxBuild(BoxLayout):
+    select_values = []
+    p_values = StringProperty()
+
+    def process_checkbox(self, widget, state, value):
+        if state == 'down':
+            self.select_values.append(value)
+            self.p_values = ','.join(self.select_values)
+        else:
+            self.select_values.remove(value)
+            self.p_values = ','.join(self.select_values)
+
+
+class MainApp(App):
+    pass
+
+
+MainApp().run()
